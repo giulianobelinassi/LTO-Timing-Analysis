10aeaf600c0f3f59e782096a310aa3861255684f
Giuliano Belinassi <giuliano.belinassi@usp.br>
2020-10-25 20:12:53 -0300

    Refactoring, and kl_exchange experiments

--- analysis_database.jl
+++ analysis_database.jl
@@ -1,15 +1,9 @@
 #!/usr/bin/julia
 using ExperimentalDesign, StatsModels, GLM, DataFrames, Distributions, Random, CSV, StatsBase, Plots, LinearAlgebra
 
-function mse(y, ŷ)
-	n = length(y)
-	diff = y - ŷ
-	acc = dot(diff, diff)
-
-	return acc/n
-end
-
 function plot_df(results)
+	mse(y1, y2) = mean((y1 - y2) .^ 2)
+
 	sort!(results, [:expected_insns])
 
 	transform!(results, AsTable(5:15) .=>
@@ -30,10 +24,10 @@ function plot_df(results)
 	mse_par4_str  = string(round(mse_parallel4, digits=2))
 	mse_par8_str  = string(round(mse_parallel8, digits=2))
 
-	println(mse_seq)
-	println(mse_parallel2)
-	println(mse_parallel4)
-	println(mse_parallel8)
+	println("MSE Sequential = " * string(mse_seq))
+	println("MSE Parallel 2 = " * string(mse_parallel2))
+	println("MSE Parallel 4 = " * string(mse_parallel4))
+	println("MSE Parallel 8 = " * string(mse_parallel8))
 
 	pltobj = plot(seq.expected_insns, seq.mean, yerror=1.96*seq.stdev/sqrt(15), label="Sequential", xaxis="Number of Instructions", yaxis="Time (s)")
 	plot!(pltobj, parallel8.expected_insns, parallel8.mean, yerror=1.96*seq.stdev/sqrt(15), label="Parallel (8 threads)")
@@ -53,37 +47,72 @@ function plot_df(results)
 	savefig("times-functions.svg")
 end
 
-results = CSV.read("output_corei7.csv", delim="; ", DataFrame)
-
-
-filter!(:Expected_insns => x -> x > 0, results)
-
 # Rename labels to lowercase, as @formula seems to not like uppercase strings.
-for name in names(results)
-	h = name => lowercase(name)
-	rename!(results, replace.(names(results), h))
+function lowercase_columns!(D)
+	for name in names(D)
+		h = name => lowercase(name)
+		rename!(D, replace.(names(D), h))
+	end
 end
 
+function kl_prediction(D, formula)
+	f = @formula(0 ~ ((expected_insns + expected_insns ^ 2) +
+												(functions + functions ^ 2) +
+												(inlined_percentage + inlined_percentage ^ 2) +
+												(num_partitions + num_partitions ^ 2)) *
+												(parallel + parallel ^ 2))
+
+
+	selected_rows = kl_exchange(f,
+								D,
+								seed_design_size = 2,
+								experiments = 11,
+								design_k = 11,
+								candidates_l = size(D, 1) - 11)
+
+	return selected_rows
+end
+
+# Open Data for analysis
+results = CSV.read("output_corei7.csv", delim="; ", DataFrame)
+
+# Rename labels to lowercase
+lowercase_columns!(results)
+
+# Filter 0-insns programs
+filter!(:expected_insns => x -> x > 0, results)
+
+# Stack data on exectution_i into multiple lines
+stacked = stack(results, Not([:filename, :functions, :expected_insns, :parallel, :inlined_percentage, :num_partitions]))
+
+# Set regression formula
+regression_formula = @formula(value ~ ((expected_insns + expected_insns ^ 2) +
+                                            (functions + functions ^ 2) +
+                                            (inlined_percentage + inlined_percentage ^ 2) +
+                                            (num_partitions + num_partitions ^ 2)) *
+                                            (parallel + parallel ^ 2))
+
+## O que fazer com isso ?
+kl = kl_prediction(stacked, regression_formula)
+##println(stacked[kl])
+
+# Set train set
 train_ratio = 0.2
 train_size = round(Int, 0.2 * nrow(results))
 train_rows = shuffle(1:nrow(results))[1:train_size]
-D = results[train_rows, :]
+train = stacked[train_rows, :]
 
+# Set test set
 #test_rows = [x for x in 1:nrow(results) if !(x in train_rows)]
-#D = results[test_rows, :]
+#test  = stacked[test_rows, :]
 
-D = stack(D, Not([:filename, :functions, :expected_insns, :parallel, :inlined_percentage, :num_partitions]))
-sort!(D, [:expected_insns, :value])
+# Fit data into test set using formula
+screening_fit = lm(regression_formula, train)
 
-regression_full = @formula(value ~ ((expected_insns + expected_insns ^ 2) +
-                                            (functions + functions ^ 2 + 1 / functions)) *
-                                            parallel * inlined_percentage)
-
-screening_model = regression_full
-screening_fit = lm(screening_model, D)
-
+# Print coefficient and R2 error
 println(screening_fit)
-println(adjr2(screening_fit))
+println("R² = " * string(adjr2(screening_fit)))
 
+# Predict data based on fitted info
 results[!, :predicted] = predict(screening_fit, results)
 plot_df(results)
